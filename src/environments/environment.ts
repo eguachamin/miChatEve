1cb79890c51c5c7e122a0952d4cf9975a0d89923
eguachamin <evelyn.guachamin@epn.edu.ec>
2025-05-06 17:08:31 -0500

    modified:   src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   //Colocar las credenciales
   firebaseConfig : {
-    apiKey: "AIzaSyD9BgflYV2qz8Amjkw5CJY6IrAqAbJNA9c",
-    authDomain: "evelyn2025-c54bf.firebaseapp.com",
-    projectId: "evelyn2025-c54bf",
-    storageBucket: "evelyn2025-c54bf.firebasestorage.app",
-    messagingSenderId: "856721939269",
-    appId: "1:856721939269:web:8f3f1c8d3a452afcdbfc2c",
-    measurementId: "G-834VZNNT7E"
+    apiKey: "AIzaSyByQgiCUzf3gYfFYcw4Pl7Rec-yRFjSIZA",
+    authDomain: "a-e2689.firebaseapp.com",
+    projectId: "a-e2689",
+    storageBucket: "a-e2689.firebasestorage.app",
+    messagingSenderId: "941773529283",
+    appId: "1:941773529283:web:8a9199b937e0fe88a88276",
+    measurementId: "G-PB8QCM0HKG"
   }
 };
 
